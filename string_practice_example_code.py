d967794a31eb3b7468cde3542aa777582346b965
Scott Jackson <scottrj@umd.edu>
2024-04-23 16:07:12 -0400

    initial commit, added some examples with quotes

---
+++ string_practice_example_code.py
@@ -0,0 +1,19 @@
+# some advanced string practice
+
+# Going over how quotes work
+"This is a string"
+'This is also a string'
+
+"This isn't a problem"
+
+# 'This isn't good'
+# "She told me "leave, please"."
+
+'She told me "leave, please".'
+
+# "She told me, "don't leave"."
+# 'She told me, "don't leave".'
+print("She told me, \"don't leave\".")
+print("This\nhas\na lot\nof\nline breaks.\n\n")
+print("\u2660")
+
